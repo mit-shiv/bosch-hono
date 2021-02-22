719b7f6d189cd8438a3de19cead68f98ba4662e3
Florian Kaltner <florian.kaltner@bosch.io>
2021-02-22 12:34:01 +0100

    #2469 Properly printing message payload in IT debug output, second try
    Signed-off-by: Florian Kaltner <florian.kaltner@bosch.io>

--- tests/src/test/java/org/eclipse/hono/tests/http/HttpTestBase.java
+++ tests/src/test/java/org/eclipse/hono/tests/http/HttpTestBase.java
@@ -547,7 +547,7 @@ public abstract class HttpTestBase {
         final VertxTestContext setup = new VertxTestContext();
 
         createConsumer(tenantId, msg -> {
-            logger.info("received {}", new String(msg.getPayload().getBytes()));
+            logger.info("received {}", msg.getPayload() != null ? msg.getPayload().toString() : null);
             ctx.verify(() -> {
                 IntegrationTestSupport.assertTelemetryMessageProperties(msg, tenantId);
                 assertThat(msg.getQos()).isEqualTo(getExpectedQoS(expectedQos));
